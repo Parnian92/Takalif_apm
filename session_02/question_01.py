3f90a185df0d327fdcd6b9ec567f56ccd1d98740
Parnian92 <parnian.db92@gmail.com>
2026-08-05 15:36:47 +0330

    Add files via upload

---
+++ session_02/question_01.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+#Homework1-Jalase 2
+
+#1) reshteyi shamel name va melli code be ham chasbide az karbar begirid ba slicing bakhshe adadi ra joda karde va ba castbe int tabdil namayid.
+data=input('enter name and melli code:')
+melli_code=int(data[-10:])
+print('melli code:',melli_code)
+=======
+#Homework1-Jalase 2
+
+#1) reshteyi shamel name va melli code be ham chasbide az karbar begirid ba slicing bakhshe adadi ra joda karde va ba castbe int tabdil namayid.
+data=input('enter name and melli code:')
+melli_code=int(data[-10:])
+print('melli code:',melli_code)
+>>>>>>> eb3e3a3be6747b59e66ccd68b6477b208cfb2f45
