929d6931e22f515a5a39cbd36bc47403100be1c8
Parnian92 <parnian.db92@gmail.com>
2026-08-05 22:45:18 +0330

    Delete session_02 directory

--- session_02/question_04.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#4) shomare hesab banki az karbar begirid va ba slicing noskhe makus aan ra chap namayid.
-account=input('enter account number:')
-print(account[::-1])
-=======
-#4) shomare hesab banki az karbar begirid va ba slicing noskhe makus aan ra chap namayid.
-account=input('enter account number:')
-print(account[::-1])
->>>>>>> eb3e3a3be6747b59e66ccd68b6477b208cfb2f45
